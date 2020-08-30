b3a7e7dcb6251a6b8bacb5fd283e49aef8d6c4b4
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-08-30 17:44:01 +0200

    Transaction added

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,5 +1,7 @@
-import React, { useContext } from "react";
-import { GlobalContext } from "../context/GlobalState";
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
@@ -9,10 +11,7 @@ export const TransactionList = () => {
       <h3>History</h3>
       <ul className="list">
         {transactions.map((transaction) => (
-          <li className="minus">
-            {transaction.text} <span> -$400</span>
-            <button className="delete-btn">x</button>
-          </li>
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
