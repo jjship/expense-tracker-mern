4ab0d18bd07786e880f5fe1fe7829bee412ea621
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-09-01 15:07:05 +0200

    production build

---
+++ client/build/precache-manifest.10f8598958202eebd8d67e2e64427dff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ac93ee551d9262ea80a1f2510354b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e7e7ad0e57df7ace593",
+    "url": "/static/css/main.d15ba9b8.chunk.css"
+  },
+  {
+    "revision": "cd6d70e514ef5d28a595",
+    "url": "/static/js/2.f92b2550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f92b2550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7e7ad0e57df7ace593",
+    "url": "/static/js/main.2d27a6d7.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
