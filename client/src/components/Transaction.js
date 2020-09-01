feedd0f4738ca493266dad6a37f6dcdd1417e8dd
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-09-01 12:06:04 +0200

    delete from db

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -12,6 +12,12 @@ export const Transaction = ({ transaction }) => {
       <span>
         {sign}${Math.abs(transaction.amount)}
       </span>
+      <button
+        onClick={() => deleteTransaction(transaction._id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
